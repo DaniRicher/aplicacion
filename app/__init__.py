134ec1a1ddc8135253553740e5ab05c79ee976ab
Daniel <richerdani3@gmail.com>
2021-12-03 12:39:22 -0500

    Corrigiendo errores

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.database.sessions import Base
-
-class Place(Base):
-    __tablename__= "places"
-    id= Column(Integer, primary_key=True, index=True)
-    name= Column(String, index=True)
-    descripcion = Column(String)
-    image = Column(String)
--- app/database/sessions.py
+++ app/database/sessions.py
@@ -5,5 +5,5 @@ from sqlalchemy.orm.session import Session
 
 from app.core.config import settings 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=false, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
