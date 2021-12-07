05938241e2416078ac6e0c2a92722d2eb6611469
Daniel <richerdani3@gmail.com>
2021-12-06 21:38:31 -0500

    Errores

--- app/database/sessions.py
+++ app/database/sessions.py
@@ -1,8 +1,7 @@
+minc
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from app.core.config  import settings   
-
-
+from app.core.config  import settings
+ 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
--- app/models/place.py
+++ app/models/place.py
@@ -6,4 +6,4 @@ class Place(Base):
    id = Column(Integer, primary_key=True, index=True)
    name = Column(String, index=True)
    description = Column(String, index=True)
-   image = Column(String)
+   image = Column(String )
