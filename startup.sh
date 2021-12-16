7de1d6e930882e183b4eeca03602021529aeba1f
Daniel <richerdani3@gmail.com>
2021-12-16 11:57:59 -0500

    Docker-Compose

--- startup.sh
+++ startup.sh
@@ -4,5 +4,5 @@
 alembic upgrade head
 
 #Start app
-poetry run uvicorn app.main:app --reload --workers 1 --host 0.0.0.0 --port 8000
-# app.main:app --host 0.0.0.0 --port 8000 --reload
+#poetry run uvicorn app.main:app --reload --workers 1 --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
