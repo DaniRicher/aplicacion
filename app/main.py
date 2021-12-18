76f6670246d3fb4c9b46d6cd28bcb3ccf2c10fb2
Daniel <richerdani3@gmail.com>
2021-12-17 19:43:05 -0500

    Docker-Compose

--- app/main.py
+++ app/main.py
@@ -1,23 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.api import api_router
 from app.core.config  import settings
- 
+from app.api.api import api_router
+
 app = FastAPI()
- 
 app.add_middleware(
    CORSMiddleware,
-   allow_origins=["*"],
+   allow_origins=["http://localhost:3000","*"],
    allow_credentials=True,
    allow_methods=["*"],
    allow_headers=["*"],
 )
- 
 app.include_router(api_router, prefix=settings.API_V1_STR)
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-    
-@app.get("/health-check")
+
+@app.get("/health-check/")
 def health_check():
-   return "OK"
+    return "Ok"
